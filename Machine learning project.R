82e49151f51bc6bb5e58900fb7da24b781782e1f
JennyDICAI <ascaidi@163.com>
2016-03-13 13:20:58 -0700

    Added files via upload

---
+++ Machine learning project.R
@@ -0,0 +1,29 @@
+##Data loading
+pmlTrain<-read.csv("pml-training.csv", header=T, na.strings=c("NA", "#DIV/0!"))
+pmlTest<-read.csv("pml-testing.csv", header=T, na.string=c("NA", "#DIV/0!"))
+## NA exclusion for all available variables
+noNApmlTrain<-pmlTrain[, apply(pmlTrain, 2, function(x) !any(is.na(x)))] 
+dim(noNApmlTrain)
+## variables with user information, time and undefined
+cleanpmlTrain<-noNApmlTrain[,-c(1:8)]
+dim(cleanpmlTrain)
+## 20 test cases provided clean info - Validation data set
+cleanpmltest<-pmlTest[,names(cleanpmlTrain[,-52])]
+dim(cleanpmltest)
+#data cleaning
+library(caret)
+inTrain<-createDataPartition(y=cleanpmlTrain$classe, p=0.75,list=F)
+training<-cleanpmlTrain[inTrain,] 
+test<-cleanpmlTrain[-inTrain,] 
+#Training and test set dimensions
+dim(training)
+dim(test)
+library(caret)
+set.seed(13333)
+fitControl2<-trainControl(method="cv", number=5, allowParallel=T, verbose=T)
+rffit<-train(classe~.,data=training, method="rf", trControl=fitControl2, verbose=F)
+predrf<-predict(rffit, newdata=test)
+confusionMatrix(predrf, test$classe)
+pred20<-predict(rffit, newdata=cleanpmltest)
+# Output for the prediction of the 20 cases provided
+pred20
